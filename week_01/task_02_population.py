09c4a634055c3664ec89ebba2e26c590e75f4d65
Quyxor <unknowfull@gmail.com>
2021-10-16 16:49:09 +0300

    [week_02] Solution examples

--- week_01/task_02_population.py
+++ week_01/task_02_population.py
@@ -1,46 +1,27 @@
-def myFirstFunction(*numbers):
-    return sum(numbers)
+# По умолчанию sorted() сортирует от меньшего к большему
+population_density = sorted([8.56, 3.5, 139, 32, 23.6, 2.8, 357])
+# Вынесу в переменным длинну списка и сумму его значений, чтобы не пересчитывать их при переиспользовании
+population_density_len = len(population_density)
+population_density_sum = sum(population_density)
 
+# Используем round() с точностью до одного знака после запятой
+density_avg = round(population_density_sum/population_density_len, 1)
+# Т.к. список со значениями начинается с наименьшего, то берем первый элемент
+density_min_by_index = population_density[0]
+# Или же, можем использовать встроенную функцию питона min()
+density_min_by_min = min(population_density)
+# Т.к. список со значениями начинается с наименьшего, то берем последний элемент
+# Кстати, можно схитрить и взять отрицательный индекс, тогда будет браться последнее значение
+density_max_by_index = population_density[-1]
+# Или же, можем использовать встроенную функцию питона max()
+density_max_by_max = max(population_density)
+# Опять же, воспольуземся отрицательным индексом
+density_top_three = population_density[-3:]
 
-def mySecondFunction(*numbers):
-    result = 1
-    for number in numbers:
-        result *= number
-    return result
+print(f"density_avg: {density_avg}")
+print(f"density_min_by_index: {density_min_by_index}")
+print(f"density_min_by_min: {density_min_by_min}")
+print(f"density_max_by_index: {density_max_by_index}")
+print(f"density_max_by_max: {density_max_by_max}")
+print(f"density_top_three: {density_top_three}")
 
-
-result = myFirstFunction(2)
-if result == 2:
-    print('Правильно :)')
-else:
-    print('Неправильно :(')
-
-result = myFirstFunction(2, 4)
-if result == 6:
-    print('Правильно :)')
-else:
-    print('Неправильно :(')
-
-result = myFirstFunction(5, 10, 15)
-if result == 30:
-    print('Правильно :)')
-else:
-    print('Неправильно :(')
-
-result = mySecondFunction(5)
-if result == 5:
-    print('Правильно :)')
-else:
-    print('Неправильно :(')
-
-result = mySecondFunction(5, 5)
-if result == 25:
-    print('Правильно :)')
-else:
-    print('Неправильно :(')
-
-result = mySecondFunction(10, 9, 2)
-if result == 180:
-    print('Правильно :)')
-else:
-    print('Неправильно :(')
